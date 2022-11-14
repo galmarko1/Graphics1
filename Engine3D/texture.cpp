4fbb5da2e214ce0ca93802a88068404579c146c0
dordolev <dolev@lightricks.com>
2022-11-14 08:39:44 +0200

    Divide sobel pixels & change threshold values.

--- Engine3D/texture.cpp
+++ Engine3D/texture.cpp
@@ -113,10 +113,10 @@ static unsigned char *sobel(unsigned char *data) {
     int len = strlen((char *) data) / 4;
     int sqr = sqrt(len);
 
-    unsigned char mat[sqr + 2][sqr + 2];
+    double mat[sqr + 2][sqr + 2];
     for (int i = 1; i < sqr + 1; i++) {
         for (int j = 1; j < sqr + 1; j++) {
-            mat[i][j] = (data[4 * (i * sqr + j)] + data[4 * (i * sqr + j) + 1] + data[4 * (i * sqr + j) + 2]) / 3;
+            mat[i][j] = (data[4 * (i * sqr + j)] + data[4 * (i * sqr + j) + 1] + data[4 * (i * sqr + j) + 2]) / 3.0;
         }
     }
 
@@ -126,22 +126,22 @@ static unsigned char *sobel(unsigned char *data) {
     /////insert gausian
 
     float gaussKernel[3][3] = {{1, 2, 1},
-                              {2, 4, 2},
-                              {1, 2, 1}};
+                               {2, 4, 2},
+                               {1, 2, 1}};
 
     for (int i = 1; i < sqr + 2; i++) {
         for (int j = 1; j < sqr + 2; j++) {
             float newPixel = mat[i - 1][j - 1] * gaussKernel[0][0] +
-                            mat[i - 1][j] * gaussKernel[0][1] +
-                            mat[i - 1][j + 1] * gaussKernel[0][2] +
-                            mat[i][j - 1] * gaussKernel[1][0] +
-                            mat[i][j] * gaussKernel[1][1] +
-                            mat[i][j + 1] * gaussKernel[1][2] +
-                            mat[i + 1][j - 1] * gaussKernel[2][0] +
-                            mat[i + 1][j] * gaussKernel[2][1] +
-                            mat[i + 1][j + 1] * gaussKernel[2][2];
+                             mat[i - 1][j] * gaussKernel[0][1] +
+                             mat[i - 1][j + 1] * gaussKernel[0][2] +
+                             mat[i][j - 1] * gaussKernel[1][0] +
+                             mat[i][j] * gaussKernel[1][1] +
+                             mat[i][j + 1] * gaussKernel[1][2] +
+                             mat[i + 1][j - 1] * gaussKernel[2][0] +
+                             mat[i + 1][j] * gaussKernel[2][1] +
+                             mat[i + 1][j + 1] * gaussKernel[2][2];
 
-            mat2[i][j] = mat[i-1][j-1] + newPixel/16;
+            mat2[i][j] = mat[i][j] + newPixel / 16.0;
         }
     }
 
@@ -152,36 +152,36 @@ static unsigned char *sobel(unsigned char *data) {
     //sobel
 //
     float gx[3][3] = {{-1, 0, 1},
-                     {-2, 0, 2},
-                     {-1, 0, 1}};
+                      {-2, 0, 2},
+                      {-1, 0, 1}};
     float gy[3][3] = {{1,  2,  1},
-                     {0,  0,  0},
-                     {-1, -2, -1}};
-
+                      {0,  0,  0},
+                      {-1, -2, -1}};
 
 
     float mat3[sqr + 2][sqr + 2];
-    float angles[sqr + 2][sqr + 2];
+    double angles[sqr + 2][sqr + 2];
 
     for (int i = 1; i < sqr + 2; i++) {
         for (int j = 1; j < sqr + 2; j++) {
             float newPixelGx = mat2[i - 1][j - 1] * gx[0][0] +
-                              mat2[i - 1][j + 1] * gx[0][2] +
-                              mat2[i + 1][j - 1] * gx[2][0] +
-                              mat2[i + 1][j + 1] * gx[2][2] +
-                              mat2[i][j - 1] * gx[1][0] +
-                              mat2[i][j + 1] * gx[1][2];
+                               mat2[i - 1][j + 1] * gx[0][2] +
+                               mat2[i + 1][j - 1] * gx[2][0] +
+                               mat2[i + 1][j + 1] * gx[2][2] +
+                               mat2[i][j - 1] * gx[1][0] +
+                               mat2[i][j + 1] * gx[1][2];
             float newPixelGy = mat2[i - 1][j - 1] * gy[0][0] +
-                              mat2[i - 1][j] * gy[0][1] +
-                              mat2[i - 1][j + 1] * gy[0][2] +
-                              mat2[i + 1][j - 1] * gy[2][0] +
-                              mat2[i + 1][j] * gy[2][1] +
-                              mat2[i + 1][j + 1] * gy[2][2];
-            float newPixel = std::sqrt(newPixelGx * newPixelGx + newPixelGy * newPixelGy);
+                               mat2[i - 1][j] * gy[0][1] +
+                               mat2[i - 1][j + 1] * gy[0][2] +
+                               mat2[i + 1][j - 1] * gy[2][0] +
+                               mat2[i + 1][j] * gy[2][1] +
+                               mat2[i + 1][j + 1] * gy[2][2];
+            float newPixel = (std::sqrt(newPixelGx * newPixelGx + newPixelGy * newPixelGy)) / 1.9;
+//            float newPixel = std::sqrt(newPixelGx * newPixelGx + newPixelGy * newPixelGy);
 
-            if(newPixel <0)
+            if (newPixel < 0)
                 mat3[i][j] = 0;
-            else if(newPixel > 255)
+            else if (newPixel > 255)
                 mat3[i][j] = 255;
             else
                 mat3[i][j] = newPixel;
@@ -190,7 +190,7 @@ static unsigned char *sobel(unsigned char *data) {
             if (newPixelGx == 0)
                 angles[i][j] = 0;
             else
-                angles[i][j] = std::atan(newPixelGy/newPixelGx);
+                angles[i][j] = (std::atan2(newPixelGy, newPixelGx)) * 180 / M_PI;
 
         }
     }
@@ -206,7 +206,7 @@ static unsigned char *sobel(unsigned char *data) {
 
     float mat4[sqr + 2][sqr + 2];
 
-    float max  = -2000;
+    float max = -2000;
     for (int i = 1; i < sqr + 2; i++) {
         for (int j = 1; j < sqr + 2; j++) {
             float p1;
@@ -215,13 +215,16 @@ static unsigned char *sobel(unsigned char *data) {
             if ((angles[i][j] <= 22.5 && angles[i][j] >= -22.5) || (angles[i][j] < -157.5 && angles[i][j] >= -180)) {
                 p1 = mat3[i][j - 1];
                 p2 = mat3[i][j + 1];
-            } else if ((angles[i][j] <= 67.5 && angles[i][j] >= 22.5) || (angles[i][j] >= -157.5 && angles[i][j] < -112.5)) {
+            } else if ((angles[i][j] <= 67.5 && angles[i][j] >= 22.5) ||
+                       (angles[i][j] >= -157.5 && angles[i][j] < -112.5)) {
                 p1 = mat3[i - 1][j - 1];
                 p2 = mat3[i + 1][j + 1];
-            } else if ((angles[i][j] <= 112.5 && angles[i][j] >= 67.5) || (angles[i][j] <-67.5 && angles[i][j] >= -112.5)) {
+            } else if ((angles[i][j] <= 112.5 && angles[i][j] >= 67.5) ||
+                       (angles[i][j] < -67.5 && angles[i][j] >= -112.5)) {
                 p1 = mat3[i - 1][j];
                 p2 = mat3[i + 1][j];
-            } else if ((angles[i][j] <= 157.5 && angles[i][j] >= 112.5) || (angles[i][j] <-22.5 && angles[i][j] >= -67.5)) {
+            } else if ((angles[i][j] <= 157.5 && angles[i][j] >= 112.5) ||
+                       (angles[i][j] < -22.5 && angles[i][j] >= -67.5)) {
                 p1 = mat3[i + 1][j - 1];
                 p2 = mat3[i - 1][j + 1];
             }
@@ -230,9 +233,8 @@ static unsigned char *sobel(unsigned char *data) {
 //                p2 = mat3[i][j + 1];
 //            }
             if (curr >= p1 && curr >= p2) {
-                if(curr > max)
+                if (curr > max)
                     max = curr;
-
                 mat4[i][j] = curr;
             } else {
                 mat4[i][j] = 0;
@@ -240,27 +242,29 @@ static unsigned char *sobel(unsigned char *data) {
         }
     }
 
-    float highThreshold = 255 * 0.2;
-    float lowThreshhold = highThreshold * 0.05;
-//    float newMat[sqr+2][sqr+2];
-    for(int i =0; i < sqr+1; i++){
-        for (int j = 0; j< sqr + 1; j++){
-            if(mat4[i][j] >= highThreshold){
+    float highThreshold = 180;
+    float lowThreshold = 60;
+
+    for (int i = 0; i < sqr + 1; i++) {
+        for (int j = 0; j < sqr + 1; j++) {
+            if (mat4[i][j] > highThreshold) {
                 mat4[i][j] = 255;
-            }else if ((mat4[i][j] <= highThreshold) & (mat4[i][j] >= lowThreshhold)){
+            } else if (mat4[i][j] > lowThreshold) {
                 mat4[i][j] = 25;
+            } else {
+                mat4[i][j] = 0;
             }
         }
     }
 
 
-    for(int i =0; i< sqr+1; i++){
-        for(int j=0; j< sqr+1; j++){
-            if (mat4[i][j] == 25){
-                if ((mat4[i+1][j-1] == 255) || (mat4[i+1][j] == 255) || (mat4[i+1][j+1] == 255)
-                        || (mat4[i][j-1] == 255) || (mat4[i][j+1] == 255)
-                        || (mat4[i-1][j-1] == 255) || (mat4[i-1][j] == 255) || (mat4[i-1][j+1] == 255))
-                        mat4[i][j] = 255;
+    for (int i = 0; i < sqr + 1; i++) {
+        for (int j = 0; j < sqr + 1; j++) {
+            if (mat4[i][j] == 25) {
+                if ((mat4[i + 1][j - 1] == 255) || (mat4[i + 1][j] == 255) || (mat4[i + 1][j + 1] == 255)
+                    || (mat4[i][j - 1] == 255) || (mat4[i][j + 1] == 255)
+                    || (mat4[i - 1][j - 1] == 255) || (mat4[i - 1][j] == 255) || (mat4[i - 1][j + 1] == 255))
+                    mat4[i][j] = 255;
                 else
                     mat4[i][j] = 0;
 //
@@ -269,24 +273,6 @@ static unsigned char *sobel(unsigned char *data) {
 //
         }
     }
-
-
-
-//  M, N = img.shape
-//    for i in range(1, M-1):
-//        for j in range(1, N-1):
-//            if (img[i,j] == weak):
-//                try:
-//                    if ((img[i+1, j-1] == strong) or (img[i+1, j] == strong) or (img[i+1, j+1] == strong)
-//                        or (img[i, j-1] == strong) or (img[i, j+1] == strong)
-//                        or (img[i-1, j-1] == strong) or (img[i-1, j] == strong) or (img[i-1, j+1] == strong)):
-//                        img[i, j] = strong
-//                    else:
-//                        img[i, j] = 0
-//                except IndexError as e:
-//                    pass
-//    return img
-//
 
     ///////////////
 
